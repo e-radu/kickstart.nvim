db15fc80c5087ef07b5baffd1b41049c8b05523c
e-radu <e-radu@dev.io>
2024-10-08 10:29:06 +0200

    added QQ and WW keymaps for fast exit and buffer write

--- init.lua
+++ init.lua
@@ -190,6 +190,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', 'QQ', ':q!<enter>', { desc = 'Quit discarding changes' })
+vim.keymap.set('n', 'WW', ':w!<enter>', { desc = 'Write buffer to file' })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
